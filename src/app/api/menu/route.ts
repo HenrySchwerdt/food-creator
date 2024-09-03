6864544aac9c785a21a829a65b34a0770de489e2
henryschwerdtner <henry.schwerdtner@web.de>
2024-09-03 14:36:56 +0200

    secured apis

--- src/app/api/menu/route.ts
+++ src/app/api/menu/route.ts
@@ -1,8 +1,12 @@
 import { type NextRequest } from "next/server";
+import { env } from "~/env";
 import { type WeekMenu } from "~/server/domain/types";
 import { insertMenu, removeAllWeekMenus } from "~/server/repository/menuRepository";
 
 const POST = async (req: NextRequest) => {
+    if (req.headers.get('Authorization') !== `Bearer ${env.REFRESH_MENU_TOKEN}`) {
+        return new Response("Unauthorized", { status: 401 });
+    }
     try {
         await removeAllWeekMenus();
         const body = await req.json() as WeekMenu;
--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,10 +1,14 @@
 import { type NextRequest } from "next/server";
+import { env } from "~/env";
 import { type Product } from "~/server/domain/types";
 import { insertWine, removeAllProducts } from "~/server/repository/productRepository";
 
 
 
 const POST = async (req: NextRequest) => {
+    if (req.headers.get('Authorization') !== `Bearer ${env.REFRESH_PRODUCT_SECRET}`) {
+        return new Response("Unauthorized", { status: 401 });
+    }
     try {
         await removeAllProducts();
         const body = await req.json() as Product[];
