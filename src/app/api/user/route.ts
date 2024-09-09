bb4211cacb96c4871b47de84a1c71fd16886661e
henryschwerdtner <henry.schwerdtner@web.de>
2024-09-09 14:34:45 +0200

    fixed bug

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -7,16 +7,19 @@ import { getUser, insertUser } from "~/server/repository/userRepository";
 const POST = async (req: NextRequest) => {
     const {userId} = auth();
     if (!userId) {
+        console.error("Unauthorized");
         return new Response("Unauthorized", { status: 401 });
     }
     const body = await req.json() as User;
     const dbUser = await getUser(userId);
     if (dbUser) {
+        console.error("User already exists");
         return new Response("User already exists", { status: 409 });
     }
     await insertUser({...body,
         id: userId
     });
+    console.error("User created with id " + userId);
     return new Response("User created", { status: 201 });
 };
 
