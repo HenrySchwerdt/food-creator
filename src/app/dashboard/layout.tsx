23ac45c9e241c9a088b82e6fdbaa5a7bc1005697
henryschwerdtner <henry.schwerdtner@web.de>
2024-09-09 10:20:09 +0200

    made whole page open but show no content

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,27 +1,29 @@
-import { UserButton } from "@clerk/nextjs";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 import { cookies } from "next/headers";
 import { type ReactNode } from "react";
 import { AppSidebar } from "~/components/app-sidebar";
 import { SidebarLayout, SidebarTrigger } from "~/components/ui/sidebar";
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return (
-    <SidebarLayout
-      defaultOpen={cookies().get("sidebar:state")?.value === "true"}
-    >
-      <AppSidebar />
-      <main className="flex flex-1 flex-col p-2 transition-all duration-300 ease-in-out">
-        <div className="h-full rounded-md border-2 border-dashed p-2">
-            <div className="w-full flex flex-row justify-between">
-                <SidebarTrigger />
-                <UserButton />
-            </div>
-          <div className="p-2">
-          {children}
-          </div>
-         
-        </div>
-      </main>
-    </SidebarLayout>
-  );
+    return (
+        <SignedIn>
+            <SidebarLayout
+                defaultOpen={cookies().get("sidebar:state")?.value === "true"}
+            >
+                <AppSidebar />
+                <main className="flex flex-1 flex-col p-2 transition-all duration-300 ease-in-out">
+                    <div className="h-full rounded-md border-2 border-dashed p-2">
+                        <div className="w-full flex flex-row justify-between">
+                            <SidebarTrigger />
+                            <UserButton />
+                        </div>
+                        <div className="p-2">
+                            {children}
+                        </div>
+
+                    </div>
+                </main>
+            </SidebarLayout>
+        </SignedIn>
+    );
 }
