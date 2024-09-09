7ba7a7cfcd45909dafa342fb44f8d325fd96da43
henryschwerdtner <henry.schwerdtner@web.de>
2024-09-09 10:40:22 +0200

    made whole page open but show no content

--- src/components/feature-settings/settings.tsx
+++ src/components/feature-settings/settings.tsx
@@ -8,15 +8,11 @@ export function Settings() {
     const [dislikedIngredients, setDislikedIngredients] = useState<string[]>([]);
     const [dietaryPreferences, setDietaryPreferences] = useState<string[]>([]);
     const [allergies, setAllergies] = useState<string[]>([]);
-    return <div className="flex flex-col gap-6">
-
+    return <div className="flex flex-col gap-6 mt-4">
         <EditableList items={favoriteIngredients} onUpdate={setFavoriteIngredients} itemLimit={20} title="Favorisierte Lebensmittel" description="Hier ist es möglich eine Liste der Zutaten zu spezfizieren, welche gerne gegessen werden und deshalb auch öfter in den Gerichten vorkommen sollen. Maximal können 20 Zutaten angeben werden. (Eier, Eisbergsalat)"/>
         <EditableList items={dislikedIngredients} onUpdate={setDislikedIngredients} itemLimit={20} title="Nicht-Favorisierte Lebensmittel" description="Hier ist es möglich eine List der Zutaten anzugeben, welche nicht gemocht werden. Maximal können 20 Zutaten angeben werden. (Eier, Eisbergsalat)" />
         <EditableList items={favoriteMeals} onUpdate={setFavoriteMeals} itemLimit={5} title="Favorisierte Gerichte" description="Hier ist es möglich eine Liste der Gerichte anzugeben, welche gerne gegessen werden und deshalb auch öfter als Gericht im Menu stehen sollen. (Pikantes Hünchen)" />
         <EditableList items={dietaryPreferences} onUpdate={setDietaryPreferences} itemLimit={3} title="Diätetische Vorlieben" description="Hier können Angaben gemacht werden zur Diät. (vegetarisch, 1 mal die Woche Fleisch, vegan)" />
         <EditableList items={allergies} onUpdate={setAllergies} title="Allergien" itemLimit={5} description="Hier können Angaben zu den jeweiligen Allergien gemacht werden, damit Lebensmittel die diese Stoffe beinhalten nicht in das Wochenmenu gelangen." />
-
-
-
     </div>;
 }
