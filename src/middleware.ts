bee9dcdf3f8d4ecf6a5fc211c5a9dbfe202f10d0
henryschwerdtner <henry.schwerdtner@web.de>
2024-09-09 15:20:30 +0200

    fixed bug

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,6 +7,6 @@ export default clerkMiddleware((auth, request) => {
 });
 
 export const config = {
-    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/"],
 };
 
