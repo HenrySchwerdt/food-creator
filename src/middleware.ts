043c3c06f0a42481e67ff9e65bb4db05512140f6
henryschwerdtner <henry.schwerdtner@web.de>
2024-09-09 09:26:34 +0200

    fixed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,8 @@
 // middleware.ts
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
-import { env } from "./env";
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
 export default clerkMiddleware((auth, request) => {
-    if (request.nextUrl.pathname == "/") {
-        // This is to prevent lawers from accessing the home page
-        const basicAuth = request.headers.get("authorization");
-        const url = request.nextUrl;
-        if (!basicAuth) {
-            url.pathname = "/api/blockHome";
-            return NextResponse.rewrite(url);
-        }
-        const authValue = basicAuth.split(" ")[1];
-        const [username, password] = Buffer.from(authValue!, "base64")
-            .toString()
-            .split(":");
-        if (username !== env.USERNAME || password !== env.PASSWORD) {
-            url.pathname = "/api/blockHome";
-            return NextResponse.rewrite(url);
-        }
-    }
     if (isProtectedRoute(request)) auth().protect();
 });
 
